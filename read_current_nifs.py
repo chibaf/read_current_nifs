817ecf394380a61bfc49b7da4fe7183f6e1100f8
chibaf <chibaf@mac.com>
2020-09-14 17:51:54 +0900

    Add files via upload

---
+++ read_current_nifs.py
@@ -0,0 +1,23 @@
+import serial, sys
+
+strPort1 = sys.argv[1]   # serial port
+
+ser1=serial.Serial(strPort1, 115200)
+
+print("connected to: " + ser1.portstr)
+
+while True:
+  try:
+    line1 = ser1.readline()
+    line2 = line1.decode('utf-8')
+    line3 = line2.strip() 
+
+    print(line3);
+
+  except KeyboardInterrupt:
+    print ('exiting')
+    break
+ser1.flush()
+ser1.close()
+
+s
